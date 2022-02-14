fbf2caf5ca8bed3ac89db7a3d2651b84d69344ca
Matthew Lowe <matthewjlowe11@gmail.com>
2022-02-14 12:29:59 -0500

    Set 'original' card array settings on home page to localStorage

--- scripts/game.js
+++ scripts/game.js
@@ -13,78 +13,31 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const game = urlParams.get('game');
 
-const arrayOriginal = [
-  {card:"2C", instruction:"Two is You!"},
-  {card:"2D", instruction:"Two is You!"},
-  {card:"2S", instruction:"Two is You!"},
-  {card:"2H", instruction:"Two is You!"},
-  {card:"3C", instruction: "Three is Me!"},
-  {card:"3D", instruction: "Three is Me!"},
-  {card:"3S", instruction: "Three is Me!"},
-  {card:"3H", instruction: "Three is Me!"},
-  {card:"4C", instruction: "Touch the Floor!"},
-  {card:"4D", instruction: "Touch the Floor!"},
-  {card:"4S", instruction: "Touch the Floor!"},
-  {card:"4H", instruction: "Touch the Floor!"},
-  {card:"5C", instruction: "Five is Guys!"},
-  {card:"5D", instruction: "Five is Guys!"},
-  {card:"5S", instruction: "Five is Guys!"},
-  {card:"5H", instruction: "Five is Guys!"},
-  {card:"6C", instruction: "Six is Girls!"},
-  {card:"6D", instruction: "Six is Girls!"},
-  {card:"6S", instruction: "Six is Girls!"},
-  {card:"6H", instruction: "Six is Girls!"},
-  {card:"7C", instruction: 'Seven Point To Heaven!'},
-  {card:"7D", instruction: 'Seven Point To Heaven!'},
-  {card:"7S", instruction: 'Seven Point To Heaven!'},
-  {card:"7H", instruction: 'Seven Point To Heaven!'},
-  {card:"8C", instruction: 'Pick a Mate!'},
-  {card:"8D", instruction: 'Pick a Mate!'},
-  {card:"8S", instruction: 'Pick a Mate!'},
-  {card:"8H", instruction: 'Pick a Mate!'},
-  {card:"9C", instruction: 'Nine is Rhyme!'},
-  {card:"9D", instruction: 'Nine is Rhyme!'},
-  {card:"9S", instruction: 'Nine is Rhyme!'},
-  {card:"9H", instruction: 'Nine is Rhyme!'},
-  {card:"10C", instruction: "Thumb Master!"},
-  {card:"10D", instruction: "Thumb Master!"},
-  {card:"10S", instruction: "Thumb Master!"},
-  {card:"10H", instruction: "Thumb Master!"},
-  {card:"JC", instruction: "Pick a Category!"},
-  {card:"JD", instruction: "Pick a Category!"},
-  {card:"JS", instruction: "Pick a Category!"},
-  {card:"JH", instruction: "Pick a Category!"},
-  {card:"QC", instruction: "Question Master!"},
-  {card:"QD", instruction: "Question Master!"},
-  {card:"QS", instruction: "Question Master!"},
-  {card:"QH", instruction: "Question Master!"},
-  {card:"KC", instruction: "Rule Change!"},
-  {card:"KD", instruction: "Rule Change!"},
-  {card:"KS", instruction: "Rule Change!"},
-  {card:"KH", instruction: "Rule Change!"},
-  {card:"AC", instruction: "Waterfall!"},
-  {card:"AD", instruction: "Waterfall!"},
-  {card:"AS", instruction: "Waterfall!"},
-  {card:"AH", instruction: "Waterfall!"},
-];
 
 let cardsArray;
+
+if(localStorage.getItem("customSettings")) {
+  cardsArray = JSON.parse(localStorage.getItem("customSettings"));
+  localstorage.setItem("cards"), JSON.stringify(cardsArray);
+} else {
+  cardsArray = localStorage.getItem("cardSettings");
+}
+
 //Set localstorage
 if(game === "new") {
-cardsArray = arrayOriginal;
+cardsArray = JSON.parse(localStorage.getItem("cardSettings"));
 localStorage.setItem("cards", JSON.stringify(cardsArray));
 } else {
 cardsArray = JSON.parse(localStorage.cards);
 //Update Deck
-  numberCards.innerHTML = 'Cards Remaining : ' + cardsArray.length;
+numberCards.innerHTML = 'Cards Remaining : ' + cardsArray.length;
 }
 
 window.addEventListener("load", () => {
 	var shuffle = new Audio("sounds/shuffleDeck.mp3");
   		let intervalID = setTimeout(() => {
- // this code runs every second
 		shuffle.play();
-}, 500);
+}, 100);
 })
 
 //OnClick Event
--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,63 @@ resumeBtn.addEventListener("click", resumeGame);
 settingsBtn.addEventListener("click", settingsGame);
 rateBtn.addEventListener("click", rateGame);
 
+const arrayOriginal = [
+  {card:"2C", instruction:"Two is You!"},
+  {card:"2D", instruction:"Two is You!"},
+  {card:"2S", instruction:"Two is You!"},
+  {card:"2H", instruction:"Two is You!"},
+  {card:"3C", instruction: "Three is Me!"},
+  {card:"3D", instruction: "Three is Me!"},
+  {card:"3S", instruction: "Three is Me!"},
+  {card:"3H", instruction: "Three is Me!"},
+  {card:"4C", instruction: "Touch the Floor!"},
+  {card:"4D", instruction: "Touch the Floor!"},
+  {card:"4S", instruction: "Touch the Floor!"},
+  {card:"4H", instruction: "Touch the Floor!"},
+  {card:"5C", instruction: "Five is Guys!"},
+  {card:"5D", instruction: "Five is Guys!"},
+  {card:"5S", instruction: "Five is Guys!"},
+  {card:"5H", instruction: "Five is Guys!"},
+  {card:"6C", instruction: "Six is Girls!"},
+  {card:"6D", instruction: "Six is Girls!"},
+  {card:"6S", instruction: "Six is Girls!"},
+  {card:"6H", instruction: "Six is Girls!"},
+  {card:"7C", instruction: 'Seven Point To Heaven!'},
+  {card:"7D", instruction: 'Seven Point To Heaven!'},
+  {card:"7S", instruction: 'Seven Point To Heaven!'},
+  {card:"7H", instruction: 'Seven Point To Heaven!'},
+  {card:"8C", instruction: 'Pick a Mate!'},
+  {card:"8D", instruction: 'Pick a Mate!'},
+  {card:"8S", instruction: 'Pick a Mate!'},
+  {card:"8H", instruction: 'Pick a Mate!'},
+  {card:"9C", instruction: 'Nine is Rhyme!'},
+  {card:"9D", instruction: 'Nine is Rhyme!'},
+  {card:"9S", instruction: 'Nine is Rhyme!'},
+  {card:"9H", instruction: 'Nine is Rhyme!'},
+  {card:"10C", instruction: "Thumb Master!"},
+  {card:"10D", instruction: "Thumb Master!"},
+  {card:"10S", instruction: "Thumb Master!"},
+  {card:"10H", instruction: "Thumb Master!"},
+  {card:"JC", instruction: "Pick a Category!"},
+  {card:"JD", instruction: "Pick a Category!"},
+  {card:"JS", instruction: "Pick a Category!"},
+  {card:"JH", instruction: "Pick a Category!"},
+  {card:"QC", instruction: "Question Master!"},
+  {card:"QD", instruction: "Question Master!"},
+  {card:"QS", instruction: "Question Master!"},
+  {card:"QH", instruction: "Question Master!"},
+  {card:"KC", instruction: "Rule Change!"},
+  {card:"KD", instruction: "Rule Change!"},
+  {card:"KS", instruction: "Rule Change!"},
+  {card:"KH", instruction: "Rule Change!"},
+  {card:"AC", instruction: "Waterfall!"},
+  {card:"AD", instruction: "Waterfall!"},
+  {card:"AS", instruction: "Waterfall!"},
+  {card:"AH", instruction: "Waterfall!"},
+];
+
+localStorage.setItem("cardSettings", JSON.stringify(arrayOriginal));
+
 function newGame() {
 	location.replace("game.html?game=new");
 }
